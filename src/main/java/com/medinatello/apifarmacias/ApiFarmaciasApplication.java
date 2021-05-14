e3fcb7a86f62e5cb78ee3092af5e6d554fbeba43
Jhoan Medina <medinatello@gmail.com>
2021-05-14 15:17:43 -0400

    init

---
+++ src/main/java/com/medinatello/apifarmacias/ApiFarmaciasApplication.java
@@ -0,0 +1,13 @@
+package com.medinatello.apifarmacias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFarmaciasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFarmaciasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medinatello/apifarmacias/ApiFarmaciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medinatello.apifarmacias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFarmaciasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
